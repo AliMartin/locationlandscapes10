a6a8ee7e7c43bedf4745c6577fe4419e857e349d
Alistair Martin <info@alimartin.com>
2023-11-30 14:54:59 +0000

    Initial setup

---
+++ sites/default/settings.local.php
@@ -0,0 +1,17 @@
+<?php
+
+$settings['update_free_access'] = FALSE;
+$settings['file_private_path'] = DRUPAL_ROOT . '/private-files';
+$settings['file_temp_path'] = DRUPAL_ROOT . '/tmp';
+
+
+
+$settings['trusted_host_patterns'] = [
+  '^locationlandscapes\.com$',
+  '^.+\.locationlandscapes\.com$',
+  '^locationlandscapes10\.ddev\.site$',
+  '^.+\.locationlandscapes10\.ddev\.site$',
+];
+
+$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
+
